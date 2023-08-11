6d14560f4bcb66f6826d70c45f7d0af80951f35a
Haile Gebregergis <Haile G@taf.local>
2023-08-11 16:22:23 +0300

    last_time

--- haile/admin.py
+++ haile/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Contract, Delier
+from .models import Contract, Delier, Deal
 
 # Register your models here.
 admin.site.register(Contract)
 admin.site.register(Delier)
+admin.site.register(Deal)
